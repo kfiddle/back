d0671df5df3c8e025bf448f8ffb9663b21f0b648
kfiddle <kenjfiddle@gmail.com>
2023-10-25 10:54:46 -0400

    gig creation, storing, testing in MongoDB

--- controllers/gig-controller.js
+++ controllers/gig-controller.js
@@ -2,19 +2,39 @@ const Gig = require('../models/gig');
 
 const controller = {};
 
-controller.getAllGigs = async (req, res, next) => {}
+controller.getAllGigs = async (req, res, next) => {
+  try {
+    let gigs = await Gig.find();
+    res.json(gigs.map((gig) => gig.toObject({ getters: true })));
+  } catch (err) {
+    console.log(err)
+    return next(new HttpError('could not retrieve all gigs from database', 404));
+  }
+};
 
-controller.createGig = async (req, res, next) => {
+controller.createGig = async (req, res) => {
+  try {
+    const { title, type, program, services } = req.body;
 
+    const newGig = new Gig({
+      title,
+      type,
+      program,
+      services,
+    });
 
-  
-}
+    const savedGig = await newGig.save();
+    res.status(201).json(savedGig);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'An error occurred saving new Gig' });
+  }
+};
 
-controller.getGigById = async (req, res, next) => {}
+controller.getGigById = async (req, res, next) => {};
 
-controller.updateGig = async (req, res, next) => {}
+controller.updateGig = async (req, res, next) => {};
 
-controller.deleteGigById = async (req, res, next) => {}
-
+controller.deleteGigById = async (req, res, next) => {};
 
 module.exports = controller;
--- models/gig.js
+++ models/gig.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const gigSchema = new mongoose.Schema({
   title: { type: String, required: true },
   type: { type: String, enum: ['pops', 'sym', 'outreach'] },
@@ -12,7 +13,7 @@ const gigSchema = new mongoose.Schema({
         required: true,
       },
       date: { type: Date, required: true },
-      startTime: { type: String, required: true }, // You can use a specific type for time or store it as a string
+      startTime: { type: String, required: true },
       endTime: { type: String }, // Optional
       notes: String,
       location: String,
@@ -22,3 +23,29 @@ const gigSchema = new mongoose.Schema({
 
 module.exports = mongoose.model("Gig", gigSchema);
 
+
+
+
+// {
+//   title: 'Sym 3- Planets',
+//   type: 'sym',
+
+
+// }
+// type: { type: String, enum: ['pops', 'sym', 'outreach'] },
+// program: [{ type: mongoose.Types.ObjectId, ref: 'Piece' }],
+// services: [
+//   {
+//     type: {
+//       type: String,
+//       enum: ['rehearsal', 'concert'], 
+//       required: true,
+//     },
+//     date: { type: Date, required: true },
+//     startTime: { type: String, required: true },
+//     endTime: { type: String }, // Optional
+//     notes: String,
+//     location: String,
+//   },
+// ],
+// });
